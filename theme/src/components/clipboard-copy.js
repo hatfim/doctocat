adf6e553cefef43de8d53d712b720bebc5701fad
hatfim <hatfim.ydniw@gmail.com>
2020-02-20 15:21:46 +0700

    update icon

--- theme/src/components/clipboard-copy.js
+++ theme/src/components/clipboard-copy.js
@@ -21,11 +21,14 @@ function ClipboardCopy({value}) {
         copy(value)
         setCopied(true)
       }}
+      style={{
+          background: 'transparent',
+          border: '0',
+        }}
     >
-      <StyledOcticon
-        icon={copied ? Check : Clippy}
-        color={copied ? 'green.5' : 'gray.7'}
-      />
+      <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" aria-hidden="true" >
+        <path d="M14,5v9H5V5h9m0-1H5A1,1,0,0,0,4,5v9a1,1,0,0,0,1,1h9a1,1,0,0,0,1-1V5a1,1,0,0,0-1-1Z"></path><path d="M2,9H1V2A1,1,0,0,1,2,1H9V2H2Z"></path>
+      </svg>
     </Button>
   )
 }
